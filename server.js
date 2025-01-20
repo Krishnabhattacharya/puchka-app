2d7ea481a082b2ee485cf4c88bbaec981c6c7b88
Krishna bhattacharya <bhattacharyakrishna2010@gmail.com>
2025-01-20 19:13:28 +0530

    fix server.js

--- server.js
+++ server.js
@@ -1,47 +1,71 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const PORT = 8000;
+// require('dotenv').config();
+
+// const Database = mongoose.connection;
+
+// Database.on('Connected',()=>{
+//     console.log("Database connected");
+// });
+
+// // starting point of the server
+
+// app.get("/",cors(),(req,res)=>{
+//     res.status(200).json(
+//         "welcome to the 8 Store API"
+//     );
+// });
+
+// var corsOptions = {
+//     origin : '*',
+//     Credential : true,
+//     optionSuccessStatus : 200,
+//     port : PORT,
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+
+
+// const routes = require("./routes");
+
+
+// app.use("/api",routes);
+
+// mongoose.connect(process.env.MONGODB_URL,).then(() => console.log("MongoDB Connected"))
+// .catch((error) => console.error("MongoDB connection error:", error));
+
+// app.listen(PORT, ()=>{
+//     console.log(
+//         `Server started at port number : ${PORT}`
+//     )
+// })
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 8000;
-require('dotenv').config();
+const routes = require('./routes'); 
 
-const Database = mongoose.connection;
-
-Database.on('Connected',()=>{
-    console.log("Database connected");
-});
-
-// starting point of the server
-
-app.get("/",cors(),(req,res)=>{
-    res.status(200).json(
-        "welcome to the 8 Store API"
-    );
-});
-
-var corsOptions = {
-    origin : '*',
-    Credential : true,
-    optionSuccessStatus : 200,
-    port : PORT,
-};
-
-app.use(cors(corsOptions));
+app.use(cors()); 
 app.use(express.json());
 
+app.use('/api', routes); 
 
-const routes = require("./routes");
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to the golgappa API");
+});
 
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
-app.use("/api",routes);
-
-mongoose.connect(process.env.MONGODB_URL,).then(() => console.log("MongoDB Connected"))
-.catch((error) => console.error("MongoDB connection error:", error));
-
-app.listen(PORT, ()=>{
-    console.log(
-        `Server started at port number : ${PORT}`
-    )
-})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server started at port number: ${PORT}`);
+});
 
 module.exports = app;
